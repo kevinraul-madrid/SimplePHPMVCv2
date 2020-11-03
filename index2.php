640528b99a12fe85a1e62075f7fbc8223d6d0c71
raul <kevinraul2000garcia@gmail.com>
2020-11-03 17:03:37 -0600

    tarea
    primer commit tarea

---
+++ index2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>manteminiento de usuarios</h1>
+    <form action="conexion.php" method="POST">
+    <input type="text" name="nombre" id="" placeholder="nombre"><br>
+    <input type="text" name="apellido" id="" placeholder="apellido"><br>
+    <input type="submit" value="adicionar">
+    </form>
+</body>
+</html>
